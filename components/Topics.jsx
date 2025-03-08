01af7b42f534d5be09cfd4c0cf9686e8d3348e61
EduardoNS1 <eduardonsantiago313@gmail.com>
2025-03-08 17:09:05 -0300

    feat: Add integration between GET and front-end

--- components/Topics.jsx
+++ components/Topics.jsx
@@ -2,20 +2,43 @@ import Link from "next/link";
 import RemoveButton from "./RemoveButton";
 import {HiPencilAlt} from "react-icons/hi";
 
-export default function TopicsList() {
-    return <>
-        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-            <div>
-                <h2 className="font-bold text-2xl">Topic Title</h2>
-                <div>Topic Description</div>
-            </div>
+const getTopics = async() => {
+    try {
+        const res = await fetch("http://localhost:3000/api/topics", {
+            cache: "no-store",
+        });
 
-            <div className="flex gap-2">
-                <RemoveButton />
-                <Link href={'/editTopic/123'}>
-                    <HiPencilAlt size={24} />
-                </Link>
-            </div>
-        </div>
-    </>
+        if (!res.ok) {
+            throw new Error('Failed to fetch topics');
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log("Error loading topics", error);
+    }
+}
+
+export default async function TopicsList() {
+
+    const { topics } = await getTopics();
+
+    return ( 
+        <>
+            {topics.map(t => (
+                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+                    <div>
+                        <h2 className="font-bold text-2xl">{t.title}</h2>
+                        <div>{t.description}</div>
+                    </div>
+
+                    <div className="flex gap-2">
+                        <RemoveButton />
+                        <Link href={`/editTopic/${t._id}`}>
+                            <HiPencilAlt size={24} />
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }
