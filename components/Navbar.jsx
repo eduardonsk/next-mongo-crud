9da12bdc4cd9cfeee57fb0116abe4798fb6f83fa
EduardoNS1 <eduardonsantiago313@gmail.com>
2025-03-23 14:00:23 -0300

    feat: Dynamic navbar based on current page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,20 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
-        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={"./"}>nsk</Link>
-            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
-        </nav>
+        <>
+            <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+                <Link className="text-white font-bold" href={"./"}>nsk</Link>
+
+                {pathname === '/' && (
+                    <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
+                )}
+            </nav>
+        </>
     )
 }
