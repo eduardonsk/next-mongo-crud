9cc18c8385398667ae26d265ae63abc93be2aa8d
EduardoNS1 <eduardonsantiago313@gmail.com>
2025-03-23 14:51:01 -0300

    style(navbar): Update navbar styles for better UI/UX

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,17 +2,18 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
 
 export default function Navbar() {
     const pathname = usePathname();
 
     return (
         <>
-            <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-                <Link className="text-white font-bold" href={"./"}>nsk</Link>
+            <nav className="bg-[#24282F] flex px-8 py-3 justify-between items-center">
+                <Link className="text-[#eaeaea] font-bold px-5 py-2.5 text-center inline-flex items-center" href={"./"}>nsk</Link>
 
                 {pathname === '/' && (
-                    <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
+                    <Link className="text-white bg-[#24292F] hover:bg-[#eaeaea]/10 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"  href={"/addTopic"}><ClipboardDocumentListIcon className="size-6 mr-2 text-[#eaeaea]" />New Topic</Link>
                 )}
             </nav>
         </>
